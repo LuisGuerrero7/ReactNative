964089359161b16aa3101019db165ea60940062c
Luis Guerrero <158310573+LuisGuerrero7@users.noreply.github.com>
2025-08-16 23:17:57 +0000

    2nd module 80%

--- first-app-RN/components/MenuItems.tsx
+++ first-app-RN/components/MenuItems.tsx
@@ -1,100 +1,99 @@
-import { View, Text, StyleSheet, FlatList, ListRenderItem, SectionList } from 'react-native';
+import { View, Text, StyleSheet, SectionList, SectionListRenderItem, SectionListData } from 'react-native';
 
-type MenuItem = {
-  title: string,
-  data: [string],
-}
+type Dish = { name: string; price: string; };
+type MenuSection = { title: string; data: Dish[] };
 
-const menuItemsToDisplay = [
+const menuItemsToDisplay: MenuSection[] = [
   {
     title: 'Appetizers',
     data: [
-      'Hummus',
-      'Moutabal',
-      'Falafel',
-      'Marinated Olives',
-      'Kofta',
-      'Eggplant Salad',
+      { name: 'Hummus', price: '$5.00' },
+      { name: 'Moutabal', price: '$5.00' },
+      { name: 'Falafel', price: '$7.50' },
+      { name: 'Marinated Olives', price: '$5.00' },
+      { name: 'Kofta', price: '$5.00' },
+      { name: 'Eggplant Salad', price: '$8.50' },
     ],
   },
   {
     title: 'Main Dishes',
-    data: ['Lentil Burger', 'Smoked Salmon', 'Kofta Burger', 'Turkish Kebab'],
+    data: [
+      { name: 'Lentil Burger', price: '$10.00' },
+      { name: 'Smoked Salmon', price: '$14.00' },
+      { name: 'Kofta Burger', price: '$11.00' },
+      { name: 'Turkish Kebab', price: '$15.50' },
+    ],
   },
   {
     title: 'Sides',
     data: [
-      'Fries',
-      'Buttered Rice',
-      'Bread Sticks',
-      'Pita Pocket',
-      'Lentil Soup',
-      'Greek Salad',
-      'Rice Pilaf',
+      { name: 'Fries', price: '$3.00'},
+      { name: 'Buttered Rice', price: '$3.00' },
+      { name: 'Bread Sticks', price: '$3.00' },
+      { name: 'Pita Pocket', price: '$3.00' },
+      { name: 'Lentil Soup', price: '$3.75' },
+      { name: 'Greek Salad', price: '$6.00' },
+      { name: 'Rice Pilaf', price: '$4.00' },
     ],
   },
   {
     title: 'Desserts',
-    data: ['Baklava', 'Tartufo', 'Tiramisu', 'Panna Cotta'],
+    data: [
+      { name: 'Baklava', price: '$3.00' },
+      { name: 'Tartufo', price: '$3.00' },
+      { name: 'Tiramisu', price: '$5.00' },
+      { name: 'Panna Cotta', price: '$5.00' },
+    ],
   },
 ];
 
-type ItemProps = {
-  name: string,
-  price: string
+type ItemProps = { name: string; price: string };
+
+function Item({ name, price }: ItemProps) {
+  return (
+    <View style={menuStyles.innerContainer}>
+      <Text style={menuStyles.itemText}>{name}</Text>
+      <Text style={menuStyles.itemText}>{price}</Text>
+    </View>
+  );
 }
 
-const Item = ({ name} : ItemProps) => (
-  <View style={menuStyles.innerContainer}>
-    <Text style={menuStyles.itemText}>{name} </Text>
-    {/* <Text style={menuStyles.itemText}>{price}</Text> */}
-  </View>
-);
+export default function MenuItems() {
+  const renderItem: SectionListRenderItem<Dish, MenuSection> = ({ item }) => (
+    <Item name={item.name} price={item.price} />
+  );
 
-
-const MenuItems = () => {
-  const renderItem: ListRenderItem<MenuItem> = ({ item }) => <Item name={item.name} price = {item.price} />;
-
-  const renderSectionHeader: ListRenderItem<MenuItem> = ({ section: title }) => (
-    <Text>{title}</Text>
-  )
+  const renderSectionHeader = ({ section }: { section: SectionListData<Dish, MenuSection> }) => (
+    <View style={menuStyles.headerStyle}>
+      <Text style={menuStyles.sectionHeader}>{section.title}</Text>
+    </View>
+  );
 
   return (
     <View style={menuStyles.container}>
-      <Text style={menuStyles.headerText}>View Menu</Text>
       <SectionList
-        keyExtractor={(item, index) => item + index}
         sections={menuItemsToDisplay}
+        keyExtractor={(item, index) => `${item.name}-${index}`}
         renderItem={renderItem}
-        renderSectionHeader={renderSectionHeader}  
-      >
-        
-      </SectionList>
+        renderSectionHeader={renderSectionHeader}
+      />
     </View>
   );
-};
+}
 
 const menuStyles = StyleSheet.create({
-  container: {
-    flex: 0.75,
-  },
+  container: { flex: 1 },
   innerContainer: {
     paddingHorizontal: 40,
     paddingVertical: 20,
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
-  itemText: {
-    color: '#F4CE14',
-    fontSize: 20,
-  },
-  headerText: {
-    color: 'white',
-    fontSize: 40,
-    flexWrap: 'wrap',
+  itemText: { color: '#F4CE14', fontSize: 20 },
+  headerStyle: { backgroundColor: '#F4CE14' },
+  sectionHeader: {
+    color: 'black',
+    fontSize: 26,
     textAlign: 'center',
-    marginVertical: 10
   },
 });
-
-export default MenuItems;
--- first-app-RN/components/WelcomeScreen.tsx
+++ first-app-RN/components/WelcomeScreen.tsx
@@ -1,20 +1,54 @@
 import * as React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { useState } from 'react';
+
+import { ScrollView, StyleSheet, Text, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 
 export default function WelcomeScreen() {
+  
+  const [firstName, setFirstName] = useState<string>('')
+  const [lastName, setLastName] = useState<string>('')
+  const [feedback, setFeedback] = useState<string>('')
+  const [phoneNumber, setPhoneNumber] = useState<string>('')
+
   return (
-    <ScrollView indicatorStyle={"white"} style={styles.container}>
-      <Text
-        style={styles.welcomeText}>
-        Welcome to Little Lemon
-      </Text>
-      <Text
-        style={styles.secondText}>
-        Little Lemon is a charming neighborhood bistro that serves simple food
-        and classic cocktails in a lively but casual environment. We would love
-        to hear more about your experience with us!
-      </Text>
-    </ScrollView>
+    <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView indicatorStyle={"white"}  keyboardDismissMode='on-drag'>
+        <Text
+          style={styles.welcomeText}>
+          Welcome to Little Lemon
+        </Text>
+        <Text
+          style={styles.secondText}>
+          Little Lemon is a charming neighborhood bistro that serves simple food
+          and classic cocktails in a lively but casual environment. We would love
+          to hear more about your experience with us!
+        </Text>
+        <Text style={styles.secondText}>
+          Please answer this questions:
+        </Text>
+        <TextInput value={firstName} onChangeText={setFirstName} placeholder='First Name'
+          style={styles.textInput}
+        >
+        </TextInput>
+        <TextInput value={lastName} onChangeText={setLastName} placeholder='LastName'
+          style={styles.textInput}
+        >
+        </TextInput>
+          <TextInput value={phoneNumber} onChangeText={setPhoneNumber} placeholder='Phone Number'
+          style={styles.textInput}
+          keyboardType='phone-pad'
+        >
+        </TextInput>
+        <TextInput value={feedback} 
+          onChangeText={setFeedback} 
+          multiline={true}
+          placeholder='Please give us your feedback!'
+          maxLength={250}
+          style={styles.feedbackInput}
+        >
+        </TextInput>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -24,16 +58,35 @@ const styles = StyleSheet.create({
   } , 
   welcomeText: {
     padding: 40,
-          fontSize: 50,
+          fontSize: 30,
           color: '#EDEFEE',
           textAlign: 'center',
   },
   secondText: {
-          fontSize: 90,
+          fontSize: 25,
           padding: 20,
           marginVertical: 8,
           color: '#EDEFEE',
           textAlign: 'center',
+  },
+  textInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    borderColor: '#EDEFEE',
+    backgroundColor: '#EDEFEE',
+  },
+  feedbackInput: {
+    height: 100,
+    margin: 12,
+    borderWidth:1,
+    padding:10,
+    fontSize: 16,
+    borderColor: '#EDEFEE',
+    backgroundColor: '#EDEFEE',
+
   }
 
 })
